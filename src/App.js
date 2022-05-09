c0208bcd52eecfdd79ca58fc77a5ced45691271a
Chris <christiantokic@hotmail.com>
2022-05-09 17:32:17 +0200

    Added newest version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { css, cx } from '@emotion/css';
 import randomColor from 'randomcolor';
 import { useState } from 'react';
 
@@ -7,14 +6,21 @@ function App() {
   const [color, setColor] = useState(randomColor());
   return (
     <div>
-      <h1 style={{ color: `${color}` }}>Hello world</h1>
+      <h1 style={{ color: { color } }}>Hello world</h1>
       <button onClick={() => setColor(randomColor())}>
         {' '}
         Create random Color
       </button>
       <div>Generated Color: {color} </div>
+      <div
+        style={{
+          display: 'flex',
+          padding: '60px',
+        }}
+      >
+        Style
+      </div>
     </div>
   );
 }
-
 export default App;
