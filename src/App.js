9cab969e7b5b4f7012bb2f134f8373b973610d93
Chris <christiantokic@hotmail.com>
2022-05-11 09:22:38 +0200

    Added new version

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         }}
       />
       generatedColor: {generateColor}
-      <div>Generated Color: background color hex code</div>'
+      <div>Generated Color: {generateColor}</div>
       <br />
       <button onClick={() => setColor(generateColor)}>Generate</button>
       <br />
